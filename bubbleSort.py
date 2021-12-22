edb4d5bd7da510d310f4d9d98732318f16d16544
Jack Adkins <90356726+adkinsj@users.noreply.github.com>
2021-12-22 16:13:57 -0600

    bubbleSort algorithm with comments

--- bubbleSort.py
+++ bubbleSort.py
@@ -3,10 +3,14 @@ import time
 def bubble_sort(data, drawData, timeSleep):
     for _ in range(len(data)-1):
         for j in range(len(data)-1):
+            # Loop through the range of data twice.
+            # Check if j is less than J+1 if it is then swap the two.
             if data[j] > data[j+1]:
                 data[j], data[j+1] = data[j+1], data[j]
+                # Calls the drawData function to color the appropriate data
                 drawData(data, ['green' if x == j or x == j+1 else 'red' for x in range(len(data))])
+                # Sleep using the time our user set in our speedScale
                 time.sleep(timeSleep)
+    # Finally we drawData to set the color to green for the range of our data
     drawData(data, ['green' for x in range(len(data))])
     
-
